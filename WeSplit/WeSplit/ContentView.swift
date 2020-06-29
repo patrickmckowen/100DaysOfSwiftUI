30c9ef0a9fb8df18b49ee90285cf6ad007adf1d6
patrickmckowen <patrick.mckowen@gmail.com>
2020-06-29 11:12:00 -0700

    Adding the first project, WeSplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by McKowen, Patrick on 6/25/20.
+//  Copyright © 2020 Patrick McKowen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var checkTotal: Double {
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let totalWithTip = orderAmount + tipValue
+        
+        return totalWithTip
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 0
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter Check Amount", text: $checkAmount)
+                        .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
+                    TextField("Enter Number of People", text: $numberOfPeople)
+                    .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
+                    
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Check Total")) {
+                    Text("$\(checkTotal, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Amount Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
